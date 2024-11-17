2c3cc9ed2d726dd914145c5d7a6b2825659ed7ba
Hans Hofner <hhofner@gmail.com>
2024-11-17 23:19:25 +0900

    ability to join group

--- lib/champions_web/live/group.ex
+++ lib/champions_web/live/group.ex
@@ -7,20 +7,55 @@ defmodule ChampionsWeb.GroupLive do
 
     is_not_current_member = !Enum.member?(group_members, user)
 
+    group = Champions.Groups.get_group!(group_id)
+    league_name = Champions.Games.get_league!(group.league_id).name
+
     {:ok,
      assign(socket,
-       group: Champions.Groups.get_group!(group_id),
+       group: group,
        group_members: group_members,
-       is_not_current_member: is_not_current_member
+       is_not_current_member: is_not_current_member,
+       league_name: league_name,
+       user_id: user.id
      )}
+  end
+
+  def handle_event("join_group", _value, socket) do
+    user_id = socket.assigns.user_id
+    group_id = socket.assigns.group.id
+
+    case Champions.Groups.create_group_membership(%{
+           user_id: user_id,
+           group_id: group_id,
+           role: "member"
+         }) do
+      {:ok, _} ->
+        socket =
+          socket
+          |> assign(is_not_current_member: false)
+          |> assign(group_members: Champions.Groups.list_group_members(group_id))
+          |> push_event("joined_group", %{})
+
+        {:noreply, socket}
+
+      {:error, _} ->
+        {:noreply, socket}
+    end
   end
 
   def render(assigns) do
     ~H"""
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-      <header class="flex gap-8">
-        <h1 class="text-2xl font-semibold mb-6"><%= @group.name %></h1>
-        <button :if={@is_not_current_member} class="btn btn-neutral btn-sm">Join</button>
+      <header class="flex gap-8 items-center">
+        <h1 class="text-2xl font-semibold"><%= @group.name %></h1>
+        <div><%= @league_name %></div>
+        <button :if={@is_not_current_member} class="btn btn-neutral btn-sm" phx-click="join_group">
+          Join
+        </button>
+        <button :if={!@is_not_current_member} class="btn btn-neutral btn-sm" disabled>
+          Leave
+        </button>
+        <div></div>
       </header>
       <section>
         <h2 class="text-lg font-semibold">Members</h2>
